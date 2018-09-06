9e6838d28fefacde0e2ad7b8f9c4a8d032a4552e
Aaron <indoorconstructionman@gmail.com>
2018-09-05 21:05:57 -0600

    Created a new view/activity for job information. Modified xml so that it uses the strings file instead of hardcode.

---
+++ app/src/main/java/com/example/dubsy/constructionestimator/ContractInformationActivity.java
@@ -0,0 +1,13 @@
+package com.example.dubsy.constructionestimator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ContractInformationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contract_information);
+    }
+}
--- app/src/main/java/com/example/dubsy/constructionestimator/UserMainActivity.java
+++ app/src/main/java/com/example/dubsy/constructionestimator/UserMainActivity.java
@@ -3,6 +3,7 @@ package com.example.dubsy.constructionestimator;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 public class UserMainActivity extends AppCompatActivity {
@@ -16,5 +17,11 @@ public class UserMainActivity extends AppCompatActivity {
         salutations = findViewById(R.id.title);
         String user = getIntent().getExtras().getString("user");
         salutations.setText("Welcome " + user + "!");
+    }
+
+
+    public void createNewContract(View view) {
+        Intent myIntent = new Intent(UserMainActivity.this, ContractInformationActivity.class);
+        UserMainActivity.this.startActivity(myIntent);
     }
 }
